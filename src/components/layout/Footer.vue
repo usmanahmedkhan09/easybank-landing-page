e2f2db48268013fc92173fd3c39f6ef767272797
usman ahmed khan <usmanahmedkhan@live.com>
2022-12-06 19:50:10 +0500

    changes in css footer included

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer class="footer">
+    <div class="logo_wrapper">
+      <Logo class="logo" />
+      <div class="social-icons">
+        <IconYouTube />
+        <IconFaceBook />
+        <IconTwitter />
+        <IconInstagram />
+        <IconPrinterest />
+      </div>
+    </div>
+  </footer>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import Logo from "@/components/icons/logoIcon.vue";
+import IconYouTube from "@/components/icons/icon-youtube.vue";
+import IconFaceBook from "@/components/icons/icon-facebook.vue";
+import IconTwitter from "@/components/icons/icon-twitter.vue";
+import IconInstagram from "@/components/icons/icon-instagram.vue";
+import IconPrinterest from "@/components/icons/icon-pinterest.vue";
+
+export default defineComponent({
+  components: {
+    Logo,
+    IconYouTube,
+    IconFaceBook,
+    IconTwitter,
+    IconInstagram,
+    IconPrinterest,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -1,14 +1,16 @@
 <template>
   <HeaderVue />
   <ContentVue />
+  <FooterVue />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import HeaderVue from "./Header.vue";
 import ContentVue from "./Content.vue";
+import FooterVue from "./Footer.vue";
 
 export default defineComponent({
-  components: { HeaderVue, ContentVue },
+  components: { HeaderVue, ContentVue, FooterVue },
   setup() {
     return {};
   },
