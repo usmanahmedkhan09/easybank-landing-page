6d4f6faa1d5dbaea04a21344c7eeae07a1f08975
usman ahmed khan <usmanahmedkhan@live.com>
2022-12-06 12:14:24 +0500

    content section is added

---
+++ src/components/layout/Content.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="content">
+    <div class="content--header">
+      <div class="details">
+        <h1>Next generation digital banking</h1>
+        <p>
+          Take you financial life online.Your Easy bank account will be
+          one-stop-shop for spending, saving, budgeting,investing,and much more
+        </p>
+        <button class="btn">Request invite</button>
+      </div>
+      <div class="icon_wrapper">
+        <img class="image" src="../../assets/images/image-mockups.png" alt="" />
+      </div>
+    </div>
+  </main>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -1,11 +1,14 @@
 <template>
   <HeaderVue />
+  <ContentVue />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import HeaderVue from "./Header.vue";
+import ContentVue from "./Content.vue";
+
 export default defineComponent({
-  components: { HeaderVue },
+  components: { HeaderVue, ContentVue },
   setup() {
     return {};
   },
